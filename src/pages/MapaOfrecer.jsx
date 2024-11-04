b7a678e1ecfdc1f0c0446b65ef8ffcec06274d5f
joaquin torres <joaquintorresconejo@gmail.com>
2024-11-04 11:18:07 -0400

    Agrege el mapa

--- src/pages/MapaOfrecer.jsx
+++ src/pages/MapaOfrecer.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
 import Button from '../components/button'
+import Map from '../assets/Mapa1.jpg'
 
 export const MapaOfrecer = () => {
+    return(
+        <>
+        <div className="pagina-ayuda">
+        <div className="contenido-izquierda">
+          <h1>¿Dónde te encuentras?</h1>
+          <p>Selecciona tu área de residencia/trabajo para mejorar tus recomendaciones</p>
+        </div>
+  
+        <div className="contenido-derecha">
+          <img src={Map} alt="Mapa" className="imagen-derecha" />
+        </div>
+        </div>
+        <div className='container button-gap'>
 
+
+        <div className='left'>
+  <Button text="Atrás" to="/" type="primary" />
+</div>
+
+<div className='right'>
+  <Button text="Siguiente" to="/mapa" type="primary" />
+</div>
+
+
+
+</div>
+        
+        </>
+        
+
+      
+          
+          
+            
+            
+          
+          )
+        
   
 
   
